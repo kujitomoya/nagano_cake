577db90e873ede12380a35a76648b04815eb8ab0
EC2 Default User <ec2-user@ip-172-31-17-159.ap-northeast-1.compute.internal>
2022-12-27 15:21:50 +0000

    [ヘッダーの調整]

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   namespace :admin do
     get "/" => "homes#top"
+    get "about" => "homes#about"
     resources :orders, only: [:show, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :edit, :create, :update]
