ba1968c1c6c06a32b466fbb8b505dd95dc487165
EC2 Default User <ec2-user@ip-172-31-17-159.ap-northeast-1.compute.internal>
2022-12-30 15:07:40 +0000

    [商品一覧ページの作成]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_path
   end
-  
+
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
